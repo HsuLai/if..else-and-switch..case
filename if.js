94dd0c50838be94f5e701c8a4a5dfd57dd4e4b8e
HsuLai <84282417+HsuLai@users.noreply.github.com>
2021-06-09 15:06:06 +0630

    Add files via upload

---
+++ if.js
@@ -0,0 +1,16 @@
+var favouriteAnimeSerie = 'Attack On Titan';
+if (favouriteAnimeSerie = 'Naruto') {
+    console.log ('This one is so popular.');
+}
+else if (favouriteAnimeSerie = 'My Hero Academia') {
+    console.log ('Yeah, it is one of my favourites ,too.');
+}
+else if (favouriteAnimeSerie = 'Attack On  Titan') {
+    console.log ('This one is so cool.');
+}
+else if (favouriteAnimeSerie = 'The Seven Deadly Sins') {
+    console.log ('Same with me!!');
+}
+else {
+    console.log ('I have never tried this one! I would try it later.');
+}
---
+++ switch .js
@@ -0,0 +1,14 @@
+var trafficLight = 'red';
+switch('trafficLight'){
+  case 'green': 
+    console.log ('Go!');
+    break;
+  case 'yellow': 
+   console.log ('Slow Down!');
+    break;
+  case 'red': 
+   console.log ('Stop!');
+    break;
+  default:
+   console.log ('Try again, something is wrong!');
+}
